bd65ca81c25312e719e33033c2c09c74db5874ff
kkito0726 <k.kito0726@gmail.com>
2023-01-20 00:03:30 +0900

    GUIからチャンネルの動画を取得できるようにした

--- public/script.js
+++ public/script.js
@@ -1,10 +1,27 @@
 const inputApiRef = document.querySelector("#api-key");
 const inputVideoIdRef = document.querySelector("#video-id");
+const inputChannelIdRef = document.querySelector("#channel-id");
+const channelSubmitButtonRef = document.querySelector("#channelSubmit");
 const submitButtonRef = document.querySelector("#submit");
 
-const URL = "https://www.googleapis.com/youtube/v3/commentThreads";
+const getVideoTitle = async (API_KEY, channelId) => {
+  const URL = "https://www.googleapis.com/youtube/v3/search";
 
-const get_comment = async (API_KEY, videoId, maxResults) => {
+  const params = {
+    key: API_KEY,
+    part: "snippet",
+    channelId: channelId,
+    maxResults: 50,
+  };
+  const query_params = new URLSearchParams(params);
+
+  const res = await fetch(`${URL}?${query_params}`);
+  const data = await res.json();
+  console.log(data);
+};
+
+const getComments = async (API_KEY, videoId, maxResults) => {
+  const URL = "https://www.googleapis.com/youtube/v3/commentThreads";
   const params = {
     key: API_KEY,
     part: "snippet",
@@ -21,11 +38,21 @@ const get_comment = async (API_KEY, videoId, maxResults) => {
   console.log(data);
 };
 
+channelSubmitButtonRef.addEventListener("click", () => {
+  const API_KEY = inputApiRef.value;
+  const channelId = inputChannelIdRef.value;
+  if (API_KEY && channelId) {
+    getVideoTitle(API_KEY, channelId, 50);
+  } else {
+    alert("入力内容が不十分です");
+  }
+});
+
 submitButtonRef.addEventListener("click", () => {
   const API_KEY = inputApiRef.value;
   const videoId = inputVideoIdRef.value;
   if (API_KEY && videoId) {
-    get_comment(API_KEY, videoId, 100);
+    getComments(API_KEY, videoId, 100);
   } else {
     alert("入力内容が不十分です");
   }
